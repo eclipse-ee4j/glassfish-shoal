5abcd1fadd09861ac4d6d19b3477545dbdd30fb2
Piotrek Zygielo <piotr.zygielo@gmail.com>
2024-12-06 22:46:38 +0100

    Use non-deprecated constructor

--- cache/src/main/java/org/shoal/ha/cache/api/DataStoreFactory.java
+++ cache/src/main/java/org/shoal/ha/cache/api/DataStoreFactory.java
@@ -1,5 +1,6 @@
 /*
  * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
+ * Copyright (c) 2024 Contributors to the Eclipse Foundation. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0, which is available at
@@ -29,7 +30,7 @@ import org.shoal.ha.mapper.DefaultKeyMapper;
 public class DataStoreFactory {
 
     public static DataStore<String, Serializable> createDataStore(String storeName, String instanceName, String groupName) {
-        DefaultKeyMapper keyMapper = new DefaultKeyMapper(instanceName, groupName);
+        DefaultKeyMapper keyMapper = new DefaultKeyMapper(instanceName);
 
         Class<Serializable> vClazz = Serializable.class;
         DataStoreContext<String, Serializable> conf = new DataStoreContext<String, Serializable>();
