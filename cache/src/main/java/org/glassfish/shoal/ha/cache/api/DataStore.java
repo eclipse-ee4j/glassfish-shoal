dcf28a3a5f4687f05a62cf0b1cccb6fb84ac8b5f
David Matějček <david.matejcek@omnifish.ee>
2026-09-17 18:55:10 +0200

    Fixed javadoc comment in DataStore
    Signed-off-by: David Matějček <david.matejcek@omnifish.ee>

--- cache/src/main/java/org/glassfish/shoal/ha/cache/api/DataStore.java
+++ cache/src/main/java/org/glassfish/shoal/ha/cache/api/DataStore.java
@@ -1,4 +1,5 @@
 /*
+ * Copyright (c) 2026 Contributors to the Eclipse Foundation.
  * Copyright (c) 1997, 2018 Oracle and/or its affiliates. All rights reserved.
  *
  * This program and the accompanying materials are made available under the
@@ -16,9 +17,13 @@
 
 package org.glassfish.shoal.ha.cache.api;
 
+import java.io.Serializable;
+
+import org.glassfish.ha.store.spi.Storable;
+
 /**
- * A DataStore allows (#{Serializable} / #{Storable}) objects to be placed in the cache. The cache itself is created and
- * configured using a #{DataStoreFactory}.
+ * A DataStore allows {@link Serializable} / {@link Storable}) objects to be placed in the cache.
+ * The cache itself is created and configured using a {@link DataStoreFactory}.
  *
  * @author Mahesh Kannan
  */
